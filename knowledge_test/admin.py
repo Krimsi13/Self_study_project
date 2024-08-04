61ef650f4a6fbd2f7377a02b3555b892ae52944f
Krimsi13 <aleksejkrimsi@gmail.com>
2024-08-04 21:23:31 +0300

    knowledge_test_models_&_admin

--- knowledge_test/admin.py
+++ knowledge_test/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from knowledge_test.models import Test, Question, Answer
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'section')
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'test')
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'answer_text', 'is_correct', 'question')
--- knowledge_test/models.py
+++ knowledge_test/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+from education.models import Section
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class Test(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название теста")
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name="Раздел", **NULLABLE)
+
+    class Meta:
+        verbose_name = "Тест"
+        verbose_name_plural = "Тесты"
+
+
+class Question(models.Model):
+    question_text = models.TextField(verbose_name="Текст вопроса")
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, verbose_name="Тест", related_name="questions", **NULLABLE)
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=255, verbose_name="Текст ответа")
+    is_correct = models.BooleanField(default=False, verbose_name="Признак правильного ответа")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Вопрос", related_name="answers",
+                                 **NULLABLE)
+
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
